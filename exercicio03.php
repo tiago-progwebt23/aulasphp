a5068310be4f3f4af81a5e2e33b8110a0305b350
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-05-04 17:32:19 -0300

    Correção Ex3

---
+++ exercicio03.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Exercício 03 (corrigido)</title>
+</head>
+<body>
+<h1>Exercício 03 (corrigido)</h1>
+<hr>
+
+<?php
+$salario = 1000;
+if($salario < 1500){
+    $novoSalario = $salario * 1.15;
+} elseif($salario <= 3000){
+    $novoSalario = $salario * 1.10;
+} else {
+    $novoSalario = $salario * 1.05;
+}
+?>
+<p>Salário antigo: <?=$salario?></p>
+<p>Salário novo: <?=$novoSalario?></p>
+
+</body>
+</html>
